b881677cad04944b8987acbfa096e7d96a504a36
iceyiyi <iceyiyi2@gmail.com>
2023-10-21 23:34:29 +0800

    Add files via upload

---
+++ HelloKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    //println("Hello World!")
+    val  h = Human()
+    h.hello()
+    //Human().hello()
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    //println("Program arguments: ${args.joinToString()}")
+}
+class Human{
+    fun hello(){
+        println("Hello")
+    }
+
+}
